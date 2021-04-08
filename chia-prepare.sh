e3ee284c7b6abba756cacb0f14d06dcc0e148745
Dal Rupnik <legoless@gmail.com>
2021-04-08 19:59:12 +0200

    Update chia-prepare.sh

--- chia-prepare.sh
+++ chia-prepare.sh
@@ -34,14 +34,14 @@ fi
 #
 
 echo '. ./activate' >> automate.sh
-echo `chia plots create -k 32 -n 1 -e -r 2 -t $DESTINATION_DIR -d $DESTINATION_DIR -f a246bb245a63442256cff84dff638a9ce54d82e0ad0fc325222420d79a93cb59bb2b5466454f61e487d1067f23b3af0f -p 8bf0193400549b3079902c2be399aad3e2d481a0c1b4dc980f23a6ea9266b028c1838da607859988d91c53c163b1fe6e` >> automate.sh
+echo "chia plots create -k 32 -n 1 -e -r 2 -t $DESTINATION_DIR -d $DESTINATION_DIR -f a246bb245a63442256cff84dff638a9ce54d82e0ad0fc325222420d79a93cb59bb2b5466454f61e487d1067f23b3af0f -p 8bf0193400549b3079902c2be399aad3e2d481a0c1b4dc980f23a6ea9266b028c1838da607859988d91c53c163b1fe6e" >> automate.sh
 
 chmod +x automate.sh
 
-echo `tmux new-session -d -s "PlotSession" './automate.sh'`
+echo "tmux new-session -d -s "PlotSession" './automate.sh'"
 
 #
 # Run tmux
 #
 
-# mux new-session -d -s "PlotSession" './automate.sh'
+# tmux new-session -d -s "PlotSession" './automate.sh'
