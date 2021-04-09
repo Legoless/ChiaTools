ac135c7cf08ad05afc6134424ce6ac17740a19d8
Dal Rupnik <legoless@gmail.com>
2021-04-09 21:17:36 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get upgrade -y -o Dpkg::Options::="--force-confold" --force-yes
 
 # Checkout the source and install
 git clone https://github.com/Chia-Network/chia-blockchain.git -b latest
